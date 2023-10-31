c4f9824b625bf392da8d030198087bc59c83f4b7
Your Name <you@example.com>
2023-10-31 00:04:18 +0000

    update

--- user_interaction_service/app.py
+++ user_interaction_service/app.py
@@ -10,31 +10,38 @@ analytics_service_url = 'http://localhost:5001'  # URL of the Analytics Service
 def home():
     return render_template('home.html')
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
 @app.route('/quote', methods=['GET'])
 def get_quote():
     # Get a random quote from the Quote Service
     quote_response = requests.get(f"{quote_service_url}/quote")
-    if quote_response.status_code == 200:
-        quote_data = quote_response.json()
-        quote_id = quote_data['id']
+    if quote_response.status_code != 200:
+        return jsonify({"error": "Quote service is unavailable"}), 503
 
-        # Get count data for the received quote ID from the Analytics Service
+    quote_data = quote_response.json()
+    quote_id = quote_data['id']
+
+    # Initialize the default quote data
+    combined_data = {
+        "id": quote_id,
+        "text": quote_data['text'],
+        "current_count": "Unavailable"
+    }
+
+    # Attempt to get count data for the received quote ID from the Analytics Service
+    try:
         analytics_response = requests.get(f"{analytics_service_url}/count/{quote_id}")
         if analytics_response.status_code == 200:
             count_data = analytics_response.json()
-            current_count = count_data['current_count']
-        else:
-            current_count = "Unavailable"
+            combined_data["current_count"] = count_data['current_count']
+    except requests.exceptions.RequestException:
+        # Log this error in real scenario
+        pass
 
-        # Combine quote and count data
-        combined_data = {
-            "id": quote_id,
-            "text": quote_data['text'],
-            "current_count": current_count
-        }
-        return jsonify(combined_data)
-    else:
-        return jsonify({"error": "Quote service is unavailable"}), 503
+    return jsonify(combined_data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002, host='0.0.0.0')
